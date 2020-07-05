6322bc3bf3955ffcda8d4746469b76fecbc6b7d4
ashkan mokarian <ashkan.mokarian@gmail.com>
2020-07-05 23:25:01 +0200

    added unet.py

--- src/test/models.py
+++ src/test/models.py
@@ -3,17 +3,27 @@ from torchsummary import summary
 
 import _init_paths
 
-from lib.models.models import ConvNet
+from lib.models.unet import UNet
+from lib.models.convnet_models import SiameseNet
 
 if __name__ == '__main__':
-    net = ConvNet(
-        [4, 16, 32, 64, 128],
-        [2, 2, 2, 2],
-        [1024, 256, 128]  # assuming 64x64x64 patches
+    # torch.cuda.empty_cache()
+    net = UNet(
+        4,
+        64,
+        4,
+        3
         )
-    input_patch = torch.randn(1, 4, 64, 64, 64)
+    input_patch = torch.randn(1, 4, 100, 100, 100)
     net = net.to("cuda:0")
     out = net(input_patch.cuda())
-    outn = out.cpu().detach().numpy()
     print(summary(net, input_patch.size()[1:]))
+
+    print('Checking Siamese memory trace')
+    torch.cuda.empty_cache()
+    siamese_unet = SiameseNet(net)
+    input2_patch = torch.randn(1,4, 100, 100, 100)
+    out1, out2 = siamese_unet(input_patch.cuda(), input2_patch.cuda())
+    input_size = input_patch.size()[1:]
+    print(summary(siamese_unet, [input_size, input_size]))
     print("finished")
